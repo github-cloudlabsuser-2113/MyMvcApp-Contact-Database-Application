37d5fb448bac9415acca107821fe3400ffd26e3e
Your Name <you@example.com>
2024-08-21 18:05:53 +0000

    app first try

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,65 +2,127 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyMvcApp.Models;
 
-namespace MyMvcApp.Controllers;
-
-public class UserController : Controller
+namespace MyMvcApp.Controllers
 {
-    public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+    public class UserController : Controller
+    {
+        public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
 
         // GET: User
         public ActionResult Index()
         {
             // Implement the Index method here
+            if (userlist != null)
+            {
+                return View(userlist);
+            }
+            else
+            {
+                return View("Error"); // or another appropriate view
+            }
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            var user = userlist?.Find(user => user.Id == id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            return View("Error"); // or another appropriate view
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            // Implement the Create method here
+            return View();
         }
 
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            try
+            {
+                // Implement the Create method here
+                userlist.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View("Error"); // or another appropriate view
+            }
         }
 
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            // Implement the Edit method here
+            var user = userlist?.Find(user => user.Id == id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            return View("Error"); // or another appropriate view
         }
 
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            try
+            {
+                // Implement the Edit method here
+                var existingUser = userlist?.Find(u => u.Id == id);
+                if (existingUser != null)
+                {
+                    existingUser.Name = user.Name;
+                    existingUser.Email = user.Email;
+                    // Update other fields as necessary
+                    return RedirectToAction(nameof(Index));
+                }
+                return View("Error"); // or another appropriate view
+            }
+            catch
+            {
+                return View("Error"); // or another appropriate view
+            }
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            var user = userlist?.Find(user => user.Id == id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            return View("Error"); // or another appropriate view
         }
 
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, IFormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            try
+            {
+                // Implement the Delete method here
+                var user = userlist?.Find(user => user.Id == id);
+                if (user != null)
+                {
+                    userlist.Remove(user);
+                    return RedirectToAction(nameof(Index));
+                }
+                return View("Error"); // or another appropriate view
+            }
+            catch
+            {
+                return View("Error"); // or another appropriate view
+            }
         }
+    }
 }
